c58500f8473684fa3d89bc414695da34527d0da0
Ye Yu <yeyuqwer@gmail.com>
2026-08-14 16:19:35 +0800

    😋 feat: add colorful development logs

--- src/lib/logger/index.ts
+++ src/lib/logger/index.ts
@@ -3,6 +3,18 @@ import { appConfig } from '#src/config/index.js'
 
 export const logger = pino({
   level: appConfig.environment === 'production' ? 'info' : 'debug',
+  transport:
+    appConfig.environment === 'development'
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            ignore: 'pid,hostname',
+            singleLine: true,
+            translateTime: 'SYS:standard',
+          },
+        }
+      : undefined,
   base: {
     service: appConfig.serviceName,
     environment: appConfig.environment,
